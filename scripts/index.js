f24a833bd9219d7d899294478e16d93e94fe8bae
Architbansal25 <63822989+Architbansal25@users.noreply.github.com>
2022-07-24 02:01:27 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'july 20, 2022',
+  birthdate: 'july 25, 2022',
   name: 'diksha'
 };
 
