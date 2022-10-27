6e56fe2849f6b3f909ab3f1108afbed6f70716c3
Architbansal25 <63822989+Architbansal25@users.noreply.github.com>
2022-10-27 21:32:43 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,7 @@ x = setInterval(function() {
     hw = w / 2, // half-width
     hh = h / 2,
     opts = {
-      strings: ['WISHING YOU A DAY THAT IS SPECIAL AS YOU ARE', 'HAPPY BIRTHDAY!', config.name],
+      strings: ['WISHING YOU A DAY THAT IS SPECIAL', 'AS YOU ARE','HAPPY BIRTHDAY!', config.name],
       charSize: 30,
       charSpacing: 35,
       lineHeight: 40,
